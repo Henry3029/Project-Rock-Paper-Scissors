cc7ed94cd4f0079386f99058840df05c99c7b452
Henry goodluck <chigoziehenry852@gmail.com>
2025-09-23 23:06:41 +0100

    add code For playRound funtion

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Hello world"); 
 
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
 	const choice = ["Rock", "Paper", "Scissors"];
 	
@@ -14,3 +17,25 @@ function getComputerChoice() {
 		return choices.toLowerCase();
 	}
 	console.log(getHumanChoices());
+	
+	function playRound(humanChoice, computerChoice) {
+		const human = humanChoice.toLowerCase();
+		const  computer = computerChoice.toLowerCase();
+		//Determine the Winner
+		if (human === computer) {
+			console.log(`Tie! both chose ${human}`); 
+			return "tie";
+		}	
+	
+	if ((human === "rock"  && computer === "Scissors") || (human === "paper" && computer === "rock") || (human === "scissors" && computer === "paper")) {
+		humanScore++ // increment humanScore
+	console.log(` You win! ${human} beats ${computer}`);
+	return "human";
+	} else {
+		computerScore++; //increment computerScore
+		console.log(`You lose! ${computer} beats ${human}`);
+		return "computer";
+	}
+	}
+		
+		
